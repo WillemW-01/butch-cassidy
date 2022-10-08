14ae4ed7d8ae86103883eaba545521734f6ce2f3
Sass, MF, Mr [22548890@sun.ac.za] <22548890@sun.ac.za>
2022-10-09 01:21:39 +0200

    added logo and removed forcast

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import GraphOrders from "../components/GraphOrders";
 import Treemap from "../components/Treemap";
 import Spinner from "../components/Spinner";
 import icon from "./add-panel.svg";
+import bulb from "./bulb.svg";
 import "./dashboard.css";
 import GraphSales from "../components/GraphSales";
 import WeekdayGraph from "../components/WeekdayGraph";
@@ -54,7 +55,7 @@ function Dashboard(props) {
               <div className="statbar item">Most orders this week: 6</div>
               <div className="statbar item">Worst performing item: Masala</div>
             </div>
-            <div className="section header">Main graphs</div>
+            <div className="section header"></div>
             <div className="main">
               <div className="main graph one">
                 <GraphOrders title="Quantities Sold" />
@@ -63,7 +64,9 @@ function Dashboard(props) {
                 <GraphSales title="Sales" />
               </div>
             </div>
-            <div className="section header">Insights</div>
+            <div className="section header">
+              <img src={bulb} width="50px" alt="logo" /> Insights
+            </div>
             <div className="insights">
               <div className="insight item">Combos</div>
               <div className="insight item">
