68f0ac30995c38a1ade973e13037cb91d53faf52
Sass, MF, Mr [22548890@sun.ac.za] <22548890@sun.ac.za>
2022-10-08 18:58:19 +0200

    improved backend functions

--- backend/analytics/urls.py
+++ backend/analytics/urls.py
@@ -3,11 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("get_orders", views.get_orders),
-    path("monthly_orders1", views.monthly_orders1),
-    path("monthly_orders2", views.monthly_orders2),
     path("monthly_items", views.monthly_items),
-    path("get_daily_orders", views.get_daily_orders),
     path("get_daily_quantities", views.get_daily_quantities),
     path("calculate_sales", views.calculate_sales),
 ]
--- backend/analytics/views.py
+++ backend/analytics/views.py
@@ -5,10 +5,10 @@ import json
 import pandas as pd
 import numpy as np
 
-orders1 = pd.read_csv("../data/restaurant-1-orders.csv")
-orders2 = pd.read_csv("../data/restaurant-2-orders.csv")
-products_price1 = pd.read_csv("../data/restaurant-1-products-price.csv")
-products_price2 = pd.read_csv("../data/restaurant-2-products-price.csv")
+# orders1 = pd.read_csv("../data/restaurant-1-orders.csv")
+# orders2 = pd.read_csv("../data/restaurant-2-orders.csv")
+# products_price1 = pd.read_csv("../data/restaurant-1-products-price.csv")
+# products_price2 = pd.read_csv("../data/restaurant-2-products-price.csv")
 
 
 def sign_up(request):
@@ -17,35 +17,10 @@ def sign_up(request):
         return JsonResponse(orders1.to_dict("records"), safe=False)
 
 
-def get_orders(request):
-    if request.method == "GET":
-        o = pd.to_datetime(orders1["Order Date"])
-        orders1["Order Date"] = o.dt.strftime("%Y-%m-%d")
-        return JsonResponse(orders1.to_dict("records"), safe=False)
-
-
-def get_daily_orders(request):
-    if request.method == "GET":
-        o = orders1[["Order Number", "Order Date"]].drop_duplicates(
-            subset=["Order Number"], keep="first"
-        )
-        o["Order Date"] = pd.to_datetime(o["Order Date"])
-        o = o.sort_values(by="Order Date", ascending=True)
-        o = o["Order Date"].dt.strftime("%Y-%m-%d")
-        # o = o.groupby(['Order Date']).count()
-        counts = o.value_counts().sort_index().to_dict()
-
-        keys = str(list(counts.keys()))
-        values = list(counts.values())
-
-        (keys, values) = zip(*counts.items())
-
-        return JsonResponse({"day": keys, "orders": values})
-
-
 def get_daily_quantities(request):
+    orders1 = pd.read_csv("../data/restaurant-1-orders.csv")
     if request.method == "GET":
-        o = orders1
+        o = orders1.copy(deep=True)
         o["Order Date"] = pd.to_datetime(o["Order Date"]).dt.strftime("%Y-%m-%d")
         o["Quantity"] = o["Quantity"].astype(int)
         o = o.groupby(["Order Date"])["Quantity"].sum().to_dict()
@@ -59,16 +34,18 @@ def get_daily_quantities(request):
 
 
 def calculate_sales(request):
+    orders1 = pd.read_csv("../data/restaurant-1-orders.csv")
     if request.method == "GET":
-        o = orders1
-        o["Product Price"] = o["Quantity"].astype(float) * o["Product Price"].astype(
-            float
-        )
+        orders1["Product Price"] = orders1["Quantity"].astype(float) * orders1[
+            "Product Price"
+        ].astype(float)
 
         # format to two decimals
-        o["Order Date"] = pd.to_datetime(o["Order Date"]).dt.strftime("%Y-%m")
+        orders1["Order Date"] = pd.to_datetime(orders1["Order Date"]).dt.strftime(
+            "%Y-%m"
+        )
 
-        ob = o.groupby(["Order Date"])["Product Price"].sum().to_dict()
+        ob = orders1.groupby(["Order Date"])["Product Price"].sum().to_dict()
 
         keys = str(list(ob.keys()))
         values = list(ob.values())
@@ -77,48 +54,8 @@ def calculate_sales(request):
         return JsonResponse({"month": keys, "sales": values})
 
 
-def monthly_orders1(request):
-    if request.method == "GET":
-        # o = orders[["Order ID", "Order Date"]].drop_duplicates(
-        #     subset=["Order ID"], keep="first"
-        # )
-        o = orders1[["Order Number", "Order Date"]].drop_duplicates(
-            subset=["Order Number"], keep="first"
-        )
-        o["Order Date"] = pd.to_datetime(o["Order Date"])
-        o = o.sort_values(by="Order Date", ascending=True)
-        o = o["Order Date"].dt.strftime("%Y-%m")
-        # o = o.groupby(['Order Date']).count()
-        counts = o.value_counts().sort_index().to_dict()
-
-        keys = str(list(counts.keys()))
-        values = list(counts.values())
-
-        (keys, values) = zip(*counts.items())
-
-        return JsonResponse({"month": keys, "orders": values})
-
-
-def monthly_orders2(request):
-    if request.method == "GET":
-        o = orders2[["Order ID", "Order Date"]].drop_duplicates(
-            subset=["Order ID"], keep="first"
-        )
-        o["Order Date"] = pd.to_datetime(o["Order Date"])
-        o = o.sort_values(by="Order Date", ascending=True)
-        o = o["Order Date"].dt.strftime("%Y-%m")
-        # o = o.groupby(['Order Date']).count()
-        counts = o.value_counts().sort_index().to_dict()
-
-        keys = str(list(counts.keys()))
-        values = list(counts.values())
-
-        (keys, values) = zip(*counts.items())
-
-        return JsonResponse({"month": keys, "orders": values})
-
-
 def monthly_items(request):
+    orders1 = pd.read_csv("../data/restaurant-1-orders.csv")
     if request.method == "GET":
 
         grouped = orders1.groupby(["Item Name"])["Quantity"].sum()
