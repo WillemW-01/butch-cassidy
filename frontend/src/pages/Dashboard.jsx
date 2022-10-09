72b560b144ddbfb783c018daaf3ed8f7d7f0e34f
Wannenburg, WP, Mr [23560762@sun.ac.za] <23560762@sun.ac.za>
2022-10-09 12:06:54 +0200

    Worked on auth page, and some design for dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,13 +4,14 @@ import GraphOrders from "../components/GraphOrders";
 import Treemap from "../components/Treemap";
 import Spinner from "../components/Spinner";
 import icon from "./add-panel.svg";
-import bulb from "./bulb.svg";
+import bulb from "./lightbulb.svg";
 import "./dashboard.css";
 import GraphSales from "../components/GraphSales";
 import WeekdayGraph from "../components/WeekdayGraph";
 
 function Dashboard(props) {
   const [showSpinner, setShowSpinner] = useState(false);
+  const [hasUploaded, setHasUploaded] = useState(false);
 
   const getData = () => {
     console.log("Got data");
@@ -19,14 +20,14 @@ function Dashboard(props) {
 
     sleep(1000).then(() => {
       setShowSpinner(false);
-      // props.setHasUploaded(true);
+      setHasUploaded(true);
       console.log("Set show to true");
     });
   };
 
   useEffect(() => {
-    console.log(`Has uploaded: ${props.hasUploaded}`);
-  }, [props.hasUploaded]);
+    console.log(`Has uploaded: ${hasUploaded}`);
+  }, [hasUploaded]);
 
   const sleep = (milliseconds) => {
     return new Promise((resolve) => setTimeout(resolve, milliseconds));
@@ -37,9 +38,9 @@ function Dashboard(props) {
       <div className="dashboard">
         <div className="header">
           <img src={icon} width="50px" alt="logo" />
-          <h1>Dashboard</h1>
+          <h1>Purple Analytics - Dashboard</h1>
           <label htmlFor="input">
-            Upload your data
+            Upload
             <input id="input" type="file" accept=".csv" onChange={getData} />
           </label>
           <div className="header logout">
@@ -48,39 +49,43 @@ function Dashboard(props) {
         </div>
 
         <div className="data body">
-          {/* {props.hasUploaded && ( */}
-          <>
-            <div className="statbar">
-              <div className="statbar item">Next holiday: Christmas Day</div>
-              <div className="statbar item">Most orders this week: 6</div>
-              <div className="statbar item">Worst performing item: Masala</div>
-            </div>
-            <div className="section header"></div>
-            <div className="main">
-              <div className="main graph one">
-                <GraphOrders title="Quantities Sold" />
+          {hasUploaded ? (
+            <>
+              <div className="statbar">
+                <div className="statbar item">Next holiday: Christmas Day</div>
+                <div className="statbar item">Most orders this week: 6</div>
+                <div className="statbar item">
+                  Worst performing item: Masala
+                </div>
               </div>
-              <div className="main graph two">
-                <GraphSales title="Sales" />
+              <div className="section header"></div>
+              <div className="main">
+                <div className="main graph one">
+                  <GraphOrders title="Quantities Sold" />
+                </div>
+                <div className="main graph two">
+                  <GraphSales title="Sales" />
+                </div>
               </div>
-            </div>
-            <div className="section header">
-              <img src={bulb} width="50px" alt="logo" /> Insights
-            </div>
-            <div className="insights">
-              <div className="insight item">Combos</div>
-              <div className="insight item">
-                <Treemap title="Item Distribution" />
+              <div className="section header">
+                <img src={bulb} width="35px" alt="logo" /> Insights
               </div>
-              <div className="insight item">
-                <WeekdayGraph title="Week Days" />
+              <div className="insights">
+                <div className="insight item">Combos</div>
+                <div className="insight item">
+                  <Treemap title="Item Distribution" />
+                </div>
+                <div className="insight item">
+                  <WeekdayGraph title="Week Days" />
+                </div>
               </div>
-            </div>
-          </>
-          {/* )} */}
+            </>
+          ) : showSpinner ? (
+            <Spinner type="spinner" />
+          ) : (
+            <div className="noData">No data yet - upload your data above.</div>
+          )}
         </div>
-
-        {showSpinner && <Spinner />}
       </div>
     </div>
   );
